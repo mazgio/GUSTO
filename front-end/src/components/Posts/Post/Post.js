62276c9dfc32b784cb0756308d0ea779dde54d51
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-12-11 09:01:29 +0100

    removed delete from business home

--- front-end/src/components/Posts/Post/Post.js
+++ front-end/src/components/Posts/Post/Post.js
@@ -13,98 +13,98 @@ import { AuthContext } from "../../../context/AuthProvider.js";
 
 //destructure Post
 const Post = ({ post, setCurrentId, sForm }, ref) => {
-    //const user = JSON.parse(localStorage.getItem('login'));
-    const [likes, setLikes] = useState(post?.likes);
-    const dispatch = useDispatch(); //initialize the dispatch to the delete action
-    const navigate = useNavigate();
-    const { currentUser } = useContext(AuthContext);
+  //const user = JSON.parse(localStorage.getItem('login'));
+  const [likes, setLikes] = useState(post?.likes);
+  const dispatch = useDispatch(); //initialize the dispatch to the delete action
+  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const userId = currentUser._id;
-    const hasLikedPost = post.likes?.find((like) => like === userId);
+  const userId = currentUser._id;
+  const hasLikedPost = post.likes?.find((like) => like === userId);
 
-    const handleLike = async () => {
-        dispatch(likePost(post._id));
+  const handleLike = async () => {
+    dispatch(likePost(post._id));
 
-        if (hasLikedPost) {
-            setLikes(post.likes.filter((id) => id !== userId));
-        } else {
-            setLikes([...post.likes, userId]);
-        }
-    };
+    if (hasLikedPost) {
+      setLikes(post.likes.filter((id) => id !== userId));
+    } else {
+      setLikes([...post.likes, userId]);
+    }
+  };
 
-    const Likes = () => {
-        if (likes && likes.length > 0) {
-            return likes.find((like) => like === userId)
-                ? (
-                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}`}</>
-                ) : (
-                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
-                );
-        }
+  const Likes = () => {
+    if (likes && likes.length > 0) {
+      return likes.find((like) => like === userId)
+        ? (
+          <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}`}</>
+        ) : (
+          <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
+        );
+    }
 
-        return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
-    };
+    return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+  };
 
-    const openPost = (e) => {
-        //  dispatch(getPost(post._id, navigate));
+  const openPost = (e) => {
+    //  dispatch(getPost(post._id, navigate));
 
-        navigate(`/posts/${post._id}`);
-    };
+    navigate(`/posts/${post._id}`);
+  };
 
-    return (
-        <Card className={classes.card} raised elevation={6}>
-            <ButtonBase
-                component="span"
-                name="test"
-                className={classes.cardAction}
-                onClick={openPost}
-                style={{ backgroundColor: '#FAE6B1 ' }}
+  return (
+    <Card className={classes.card} raised elevation={6}>
+      <ButtonBase
+        component="span"
+        name="test"
+        className={classes.cardAction}
+        onClick={openPost}
+        style={{ backgroundColor: '#FAE6B1 ' }}
+      >
+        <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
+        <div className={classes.overlay}>
+          <Typography variant="h6">{post.name}</Typography>
+          <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+        </div>
+        {(currentUser.result?._id === post?.creator) && (
+          <div className={classes.overlay2} name="edit">
+            <Button 
+              onClick={(e) => {
+                sForm.current.scrollIntoView({ behavior: "smooth" });
+                e.stopPropagation();
+                console.log("something");
+                setCurrentId(post._id);
+              }}
+              style={{ color: 'white' }}
+              size="small"
             >
-                <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
-                <div className={classes.overlay}>
-                    <Typography variant="h6">{post.name}</Typography>
-                    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-                </div>
-                {(currentUser.result?._id === post?.creator) && (
-                    <div className={classes.overlay2} name="edit">
-                        <Button
-                            onClick={(e) => {
-                                sForm.current.scrollIntoView({ behavior: "smooth" });
-                                e.stopPropagation();
-                                console.log("something");
-                                setCurrentId(post._id);
-                            }}
-                            style={{ color: 'white' }}
-                            size="small"
-                        >
-                            <MoreHorizIcon fontSize="medium" />
-                        </Button>
-                    </div>
-                )}
-                <div className={classes.details}>
-                    <Typography style={{ marginLeft: '10px', color: '#31525B' }} variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-                </div>
-                <Typography style={{ color: '#31525B' }} className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 10).join(' ')}... <b style={{ color: 'black' }}>read more</b></Typography>
-                </CardContent>
-            </ButtonBase>
-            <CardActions style={{ padding: '4px', backgroundColor: '#FAE6B1' }} className={classes.cardActions}>
-                <Button style={{ padding: '4px', color: '#31525B ' }} size="medium" color="primary" disabled={userId === null} onClick={handleLike}>
-                    <Likes />
-                </Button>
+              <MoreHorizIcon fontSize="medium" />
+            </Button>
+          </div>
+        )}
+        <div className={classes.details}>
+          <Typography style={{ marginLeft: '10px', color: '#31525B' }} variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+        </div>
+        <Typography style={{ color: '#31525B' }} className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 10).join(' ')}... <b style={{ color: 'black' }}>read more</b></Typography>
+        </CardContent>
+      </ButtonBase>
+      <CardActions style={{ padding: '4px', backgroundColor: '#FAE6B1' }} className={classes.cardActions}>
+        <Button style={{ padding: '4px', color: '#31525B ' }} size="medium" color="primary" disabled={userId === null} onClick={handleLike}>
+          <Likes />
+        </Button>
 
-                {(currentUser.result === post?.creator) && (
-                    currentUser.userType !== "customerUsers" && currentUser._id != null ?
-                        (<Button size="small" color="secondary" disabled={userId === null} onClick={() => dispatch(deletePost(post._id))}>
-                            <DeleteIcon fontSize="small" /> &nbsp; Delete
-                        </Button>) : (<></>)
-                )}
-            </CardActions>
-        </Card >
-    );
+        {(currentUser.result === post?.creator) && (
+          currentUser.userType !== "customerUsers" && currentUser._id != null ?
+            (<Button className={classes.button} size="small" color="secondary" disabled={userId === null} onClick={() => dispatch(deletePost(post._id))}>
+              <DeleteIcon fontSize="small" /> &nbsp;
+            </Button>) : (<></>)
+        )}
+      </CardActions>
+    </Card >
+  );
 };
 
 export default React.forwardRef(Post);
