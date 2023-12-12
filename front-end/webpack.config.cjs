188dd0da00e49500a296753f5503b67112a2b4dd
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-12-12 17:03:29 +0100

    type commonjs

--- front-end/webpack.config.cjs
+++ front-end/webpack.config.cjs
@@ -1,14 +1,9 @@
+const path = require('path');
+
 module.exports = {
-  // ... other webpack configurations
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
+  // other webpack configurations...
+  resolve: {
+    extensions: ['.js', '.jsx'], // Add any other extensions you are using
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
   },
 };
