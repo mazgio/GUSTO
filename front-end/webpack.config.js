edc4745c91bceac9513ccfdc7d906e85db994466
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-12-12 01:10:24 +0100

    added webpack.cofig,js

---
+++ front-end/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // ... other webpack configurations
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
