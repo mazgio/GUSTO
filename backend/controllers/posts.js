73489763a8b3d54107457451ac9491411919088e
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2024-01-13 11:36:29 +0100

    clean code app.js

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -5,137 +5,136 @@ import PostMessage from '../models/postMessage.js';
 const router = express.Router();
 
 export const getPosts = async (req, res, next) => {
-    const { page } = req.query;
+  const { page } = req.query;
 
-    try {
-        const LIMIT = 8;
-        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
 
-        const total = await PostMessage.countDocuments({});
-        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
-
-        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+    res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 
 };
 
 export const getPostsBySearch = async (req, res) => {
-    const { searchQuery, tags } = req.query;
-    try {
-        const title = new RegExp(searchQuery, "i");
-        console.log(searchQuery);
+  const { searchQuery, tags } = req.query;
+  try {
+    const title = new RegExp(searchQuery, "i");
+    console.log(searchQuery);
 
-        const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(',') } }] });
-        res.json({ data: posts });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(',') } }] });
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 
 };
 
 export const getPostsByCreator = async (req, res) => {
-    const { name } = req.query;
-    // Block "try" if it is successful and "catch" if we have an error
-    try {
-        const posts = await PostMessage.find({ name });
+  const { name } = req.query;
+  // Block "try" if it is successful and "catch" if we have an error
+  try {
+    const posts = await PostMessage.find({ name });
 
-        res.json({ data: posts });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 export const getPost = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const post = await PostMessage.findById(id);
+  try {
+    const post = await PostMessage.findById(id);
 
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
 
 
-    }
+  }
 };
 
 export const createPost = async (req, res) => {
-    const post = req.body; // access to a req.body for different post
+  const post = req.body; // access to a req.body for different post
 
-    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+  const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
 
-    try {
-        await newPostMessage.save();  //asynchronous function to save a Post
+  try {
+    await newPostMessage.save();  //asynchronous function to save a Post
 
-        res.status(201).json(newPostMessage);  //201 created
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
+    res.status(201).json(newPostMessage);  //201 created
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
 
 export const updatePost = async (req, res) => {
-    const { id } = req.params; //extract the id, obj destructuring
-    const { title, message, selectedFile, tags, name } = req.body; //we´re receiving all the data for the updates (sent from the frontend)
-    //check to see if id is really a mongoose object id
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  const { id } = req.params; //extract the id, obj destructuring
+  const { title, message, selectedFile, tags, name } = req.body; //we´re receiving all the data for the updates (sent from the frontend)
+  //check to see if id is really a mongoose object id
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = { name, title, message, tags, selectedFile, _id: id }; //or we can spread the post and add the _id
+  const updatedPost = { name, title, message, tags, selectedFile, _id: id }; //or we can spread the post and add the _id
 
-    //To call our model which is the post message, we´re going to call a method (where we pass the id)
-    let updatedDBPost = await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+  //To call our model which is the post message, we´re going to call a method (where we pass the id)
+  let updatedDBPost = await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
 
-    res.json(updatedDBPost);
+  res.json(updatedDBPost);
 };
 
 export const deletePost = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    await PostMessage.findByIdAndRemove(id);
+  await PostMessage.findByIdAndRemove(id);
 
 
-    res.json({ message: "Post deleted successfully." });
+  res.json({ message: "Post deleted successfully." });
 };
 
 export const likePost = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!req.userId) {
-        return res.json({ message: "Unauthenticated" });
-    }
+  if (!req.userId) {
+    return res.json({ message: "Unauthenticated" });
+  }
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const post = await PostMessage.findById(id); //find the post that we are looking for
+  const post = await PostMessage.findById(id); //find the post that we are looking for
 
-    const index = post.likes.findIndex((id) => id === String(req.userId));
+  const index = post.likes.findIndex((id) => id === String(req.userId));
 
-    if (index === -1) {
-        post.likes.push(req.userId);
-    } else {
-        post.likes = post.likes.filter((id) => id !== String(req.userId));
-    }
-    await post.save();
-    // const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+  await post.save();
+  // const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
 
-    res.status(200).json(post);
+  res.status(200).json(post);
 };
 
 export const commentPost = async (req, res) => {
-    const { id } = req.params;
-    const { value } = req.body;
+  const { id } = req.params;
+  const { value } = req.body;
 
-    const post = await PostMessage.findById(id);
+  const post = await PostMessage.findById(id);
 
-    post.comments.push(value);
+  post.comments.push(value);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
-    res.json(updatedPost);
+  res.json(updatedPost);
 };
 
 export default router;
