98eee25689af1bf049fb373f334a034eff4f3d80
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2024-01-13 11:37:13 +0100

    cleaning code app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.js";
 import Home from "./components/pages/Home/Home.js";
@@ -18,50 +18,28 @@ import Logout from "./components/Logout.jsx";
 import { Dashboard1 } from "./components/pages/Dashboard/AboutUser.jsx";
 import { Dashboard2 } from "./components/pages/Dashboard/ServicesUser.jsx";
 import PostDetails from './components/PostDetails/PostDetails.jsx';
-// import HomeSearch from './components/Home/HomeSearch.js';
-// import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag.jsx';
 import ContainerSearch from "./SearchPage.js";
 import Success from "./components/pages/Contact/success.js";
 import Team from "./components/pages/About/Team.js";
 
 
 function App() {
-  //const [currentUserId, setCurrentUserId] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showLogin, setShowLogin] = useState(true);
-
 
   const { currentUser, setCurrentUser } = useContext(AuthContext);
 
-  // logout button
-  const logout = () => {
-    localStorage.removeItem("data");
-    // setToken(false);
-    setCurrentUser("");
-    setIsLoggedIn(false);
-    setShowLogin(true);
-
-  };
   return (
     <div className="home">
-
-
       <Navbar />
       <div className="container">
         <Routes>
-          {/* <Route path="/" element={<Navigate to='/' />} /> */}
           <Route exact path="/" element={<Navigate replace to="/home" />} />
           <Route path="/home" element={currentUser._id ? <Dashboard /> : <Home />} />
           <Route path="/posts" element={<ContainerSearch />} />
-          {/* <Route path="/posts/search" component={<HomeSearch />} /> */}
           <Route path="/posts/:id" element={<PostDetails />} />
-          {/* <Route path={['/creators/:name', '/tags/:name']} component={<CreatorOrTag />} /> */}
           <Route path="/auth" exact component={() => (!currentUser ? <AuthProvider /> : <Navigate to="/home" />)} />
           <Route path="/about" element={currentUser._id ? <Dashboard1 /> : <About />} />
           <Route path="/team" element={<Team />} />
-
           <Route path="/services" element={currentUser._id ? <Dashboard2 /> : <Services />} />
-          {/* <Route path="/rating" element={<Rating />} /> */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/signin" element={<Signin setCurrentUser={setCurrentUser} />} />
           <Route path="/signup" element={<Signup />} />
@@ -76,7 +54,6 @@ function App() {
         </Routes>
       </div>
       <Footer />
-
     </div >
   );
 }
