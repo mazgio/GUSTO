326a525d75cdaa478acf895cea4d3d3d72959e49
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2024-01-17 12:46:53 +0100

    redirected to signin from register

--- front-end/src/components/pages/Signup/CustomerSignup.js
+++ front-end/src/components/pages/Signup/CustomerSignup.js
@@ -81,114 +81,115 @@ const CustomerSignup = (props) => {
       }
     } catch (err) {
       alert(err.message);
-    };
-
-
-
-    const validate = (values) => {
-      const errors = {};
-      // const regex =
-      //   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-      if (!values.firstname) {
-        errors.firstname = "First name is required";
-      }
-
-      if (!values.lastname) {
-        errors.lastname = "Last name is required";
-      }
-
-      if (!values.username) {
-        errors.username = "Username is required";
-      }
-
-      if (!values.emailAddress) {
-        errors.emailAddress = "Email is required";
-      }
-
-      if (!values.password) {
-        errors.password = "Password is required";
-      }
-
-      return errors;
-    };
-
-    const navigate = useNavigate();
-
-    return (
-      <div className="signup-container">
-        {Object.keys(formErrors).length === 0 && isSubmit ? (
-          navigate("/signin")
-        ) : (
-          <></>
-        )}
-        <form onSubmit={handleSubmit}>
-          <h1>Sign Up</h1>
-          <hr />
-          <div className="form">
-            <div className="field">
-              <label>First Name</label>
-              <input
-                type="text"
-                name="firstname"
-                placeholder="First Name"
-                value={formValues.firstname}
-                onChange={handleChange}
-              />
-            </div>
-            <p className="err">{formErrors.firstname}</p>
-            <div className="field">
-              <label>Last Name</label>
-              <input
-                type="text"
-                name="lastname"
-                placeholder="Last Name"
-                value={formValues.lastname}
-                onChange={handleChange}
-              />
-            </div>
-            <p className="err">{formErrors.lastname}</p>
-            <div className="field">
-              <label>Username</label>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                value={formValues.username}
-                onChange={handleChange}
-              />
-            </div>
-            <p className="err">{formErrors.username}</p>
-            <div className="field">
-              <label>Email</label>
-              <input
-                type="email"
-                name="emailAddress"
-                placeholder="Email"
-                value={formValues.emailAddress}
-                onChange={handleChange}
-              />
-            </div>
-            <p className="err">{formErrors.emailAddress}</p>
-            <div className="field">
-              <label>Password</label>
-              <div className="pass-eye">
-                <input
-                  type={type}
-                  name="password"
-                  placeholder="Password"
-                  value={formValues.password}
-                  onChange={handleChange}
-                />
-                <span onClick={handleToggle} className="eye"><Icon icon={icon} /></span>
-              </div>
-            </div>
-            <p className="err">{formErrors.password}</p>
-            <button>Sign Up</button>
-          </div>
-        </form>
-      </div>
-    );
+    }
   };
 
-  export default CustomerSignup;
+
+
+  const validate = (values) => {
+    const errors = {};
+    // const regex =
+    //   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (!values.firstname) {
+      errors.firstname = "First name is required";
+    }
+
+    if (!values.lastname) {
+      errors.lastname = "Last name is required";
+    }
+
+    if (!values.username) {
+      errors.username = "Username is required";
+    }
+
+    if (!values.emailAddress) {
+      errors.emailAddress = "Email is required";
+    }
+
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+
+    return errors;
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="signup-container">
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        navigate("/signin")
+      ) : (
+        <></>
+      )}
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <hr />
+        <div className="form">
+          <div className="field">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+              value={formValues.firstname}
+              onChange={handleChange}
+            />
+          </div>
+          <p className="err">{formErrors.firstname}</p>
+          <div className="field">
+            <label>Last Name</label>
+            <input
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+              value={formValues.lastname}
+              onChange={handleChange}
+            />
+          </div>
+          <p className="err">{formErrors.lastname}</p>
+          <div className="field">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={handleChange}
+            />
+          </div>
+          <p className="err">{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              name="emailAddress"
+              placeholder="Email"
+              value={formValues.emailAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <p className="err">{formErrors.emailAddress}</p>
+          <div className="field">
+            <label>Password</label>
+            <div className="pass-eye">
+              <input
+                type={type}
+                name="password"
+                placeholder="Password"
+                value={formValues.password}
+                onChange={handleChange}
+              />
+              <span onClick={handleToggle} className="eye"><Icon icon={icon} /></span>
+            </div>
+          </div>
+          <p className="err">{formErrors.password}</p>
+          <button>Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CustomerSignup;
