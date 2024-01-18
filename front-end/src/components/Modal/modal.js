b16c0cfd5e448e43dac49aa333601c36798cfa37
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2024-01-18 10:55:34 +0100

    fixed join

--- front-end/src/components/Modal/modal.js
+++ front-end/src/components/Modal/modal.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useCallback } from 'react';
 import { useSpring, animated } from 'react-spring';
 import styled from 'styled-components';
 import { MdClose } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const Background = styled.div`
   width: 100%;
@@ -120,63 +121,63 @@ const CloseModalButton = styled(MdClose)`
 `;
 
 export const Modal = ({ showModal, setShowModal }) => {
-    const modalRef = useRef();
+  const modalRef = useRef();
 
-    const animation = useSpring({
-        config: {
-            duration: 250
-        },
-        opacity: showModal ? 1 : 0,
-        transform: showModal ? `translateY(0%)` : `translateY(-100%)`
-    });
+  const animation = useSpring({
+    config: {
+      duration: 250
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+  });
 
-    const closeModal = e => {
-        if (modalRef.current === e.target) {
-            setShowModal(false);
-        }
-    };
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
 
 
-    const keyPress = useCallback(
-        e => {
-            if (e.key === 'Escape' && showModal) {
-                setShowModal(false);
-                console.log('I pressed');
-            }
-        },
-        [setShowModal, showModal]
-    );
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+        console.log('I pressed');
+      }
+    },
+    [setShowModal, showModal]
+  );
 
-    useEffect(
-        () => {
-            document.addEventListener('keydown', keyPress);
-            return () => document.removeEventListener('keydown', keyPress);
-        },
-        [keyPress]
-    );
+  useEffect(
+    () => {
+      document.addEventListener('keydown', keyPress);
+      return () => document.removeEventListener('keydown', keyPress);
+    },
+    [keyPress]
+  );
 
-    return (
-        <>
-            {showModal ? (
-                <Background onClick={closeModal} ref={modalRef}>
-                    <animated.div style={animation}>
-                        <ModalWrapper showModal={showModal}>
+  return (
+    <>
+      {showModal ? (
+        <Background onClick={closeModal} ref={modalRef}>
+          <animated.div style={animation}>
+            <ModalWrapper showModal={showModal}>
 
-                            <ModalImg src={require('../images/pancakes.jpg')} alt='camera' />
-                            <ModalContent >
-                                <h1>Are you ready?</h1>
-                                <p>Get our exclusive food.</p>
-                                <button ><a href="signup">Join Now</a></button>
-                            </ModalContent>
-                            <CloseModalButton
-                                aria-label='Close modal'
-                                onClick={() => setShowModal(prev => !prev)}
-                            />
+              <ModalImg src={require('../images/pancakes.jpg')} alt='camera' />
+              <ModalContent >
+                <h1>Are you ready?</h1>
+                <p>Get our exclusive food.</p>
+                <button ><Link to="/signup" >Join Now</Link></button>
+              </ModalContent>
+              <CloseModalButton
+                aria-label='Close modal'
+                onClick={() => setShowModal(prev => !prev)}
+              />
 
-                        </ModalWrapper>
-                    </animated.div>
-                </Background>
-            ) : null}
-        </>
-    );
+            </ModalWrapper>
+          </animated.div>
+        </Background >
+      ) : null}
+    </>
+  );
 };
