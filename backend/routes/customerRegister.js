360c51cfaeefb854d7bdcf10f0223394511873e6
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2024-01-18 10:37:15 +0100

    fixed middleware

--- backend/routes/customerRegister.js
+++ backend/routes/customerRegister.js
@@ -1,10 +1,8 @@
-// controllers/customerRegister.js
 import express from "express";
 import requiredValues from "../validators/requiredValues.js";
 import checkValidation from "../validators/checkValidation.js";
 import registerCustomerValidator from "../validators/registerCustomerValidator.js";
 import { registerCustomerPost } from "../controllers/customerRegisterController.js";
-
 
 const router = express.Router();
 
@@ -13,6 +11,6 @@ router.get("/", (req, res) => {
   res.send("I am the register customer route");
 });
 
-router.post("/", requiredValues(["username", "password", "emailAddress"]), checkValidation, registerCustomerValidator, registerCustomerPost);
+router.post("/", requiredValues(["username", "password", "emailAddress"]), registerCustomerValidator, checkValidation, registerCustomerPost);
 
 export default router;
