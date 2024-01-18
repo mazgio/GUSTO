2a60f1d94c59312d2a79dc1b7babb577ee4ba939
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2024-01-18 10:47:15 +0100

    fixed Link /customer and /business

--- front-end/src/components/pages/Signup/Signup.js
+++ front-end/src/components/pages/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Signup.css";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
@@ -7,16 +8,16 @@ const Signup = () => {
       <div className="main-signup">
         <h2>Choose an account</h2>
         <div className="sign-box">
-          <a href="/business">
+          <Link to="/business">
             <h3>Business account</h3>
             <p>We are aiming to upgrade your business and help you to reach out more customers.</p>
-          </a>
+          </Link>
         </div>
         <div className="sign-box">
-          <a href="/customer">
+          <Link to="/customer">
             <h3>Customer</h3>
             <p>We're giving you the most easy way to reach out what you are looking for.</p>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
